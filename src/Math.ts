8aadc5d21d03f6ef9dcbeadcf7ebdfaf3e321dd9
Kenny Kwan <macbookpro@kenny-kwan.local>
2017-09-17 23:41:07 -0700

    Before changing rp

--- src/Math.ts
+++ src/Math.ts
@@ -3,6 +3,8 @@
  */
 
 import Log from "./Util";
+import rp = require('request-promise-native');
+
 
 interface IMath {
     getJSON(urls: string[]): Promise<Array<any>>;
@@ -16,10 +18,35 @@ export default class Math implements IMath {
 
     getJSON(urls: string[]): Promise<Array<any>> {
         return new Promise(function (fulfill, reject) {
-            // TODO: implement
-            // Uncommenting the line below should make one test pass
-            // fulfill([]);
-        });
+            var pArr : Array<any> = [];
+            for (let url of urls) {
+                rp('url').then(function (JSONbody) {
+                    console.log(url);
+                    console.log(JSONbody);
+                    pArr.push(new Promise(function (fulfill, reject) {
+                        fulfill(JSONbody);
+                    }))
+                }).catch(function (err) {
+                    //URL ERROR
+                    console.error('Error: URL could not be retrieved' + err);
+                })
+            }
+            fulfill();
+            /*
+            // Parse array of JSON
+            Promise.all(pArr).then(function (result) {
+                try {
+                    let i = JSON.parse(result);
+                    fulfill(i);
+                } catch (Error) {
+                    reject('Error: JSON could not be parsed')
+                }
+            }).catch(function (err) {
+                Error('Error: URL could not be retrieved');
+
+            }); */
+        })
+
     }
 
     add(urls: string[]): Promise<number> {
